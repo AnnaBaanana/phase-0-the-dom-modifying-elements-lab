cd9b5a6404ab3b4be00b21de9624f3954d92b8b5
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-04-15 15:51:10 -0400

    commit tree 02ae84925e09a24a0067b1b2514fff42bf11ae2f

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.getElementById('main').remove()
+const h1 = document.createElement('h1')
+h1.id = 'victory'
+
+document.body.append(h1)
+
+const newHeader = document.querySelector('h1#victory')
+
+newHeader.textContent = 'Anna is the champion'
